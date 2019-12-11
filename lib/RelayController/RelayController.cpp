a9b6ae8e1e372f1d2ce5e4825bc7a9525dff044c
Carsten Grønning <carsten.groenning@gmail.com>
2019-12-11 18:43:03 +0100

    added standard trigger functionality

--- lib/RelayController/RelayController.cpp
+++ lib/RelayController/RelayController.cpp
@@ -5,10 +5,10 @@
 #include "RelayController.h"
 #include <Wire.h>
 
-RelayController::RelayController(input_t inputs=NUM_OF_INPUTS, trigger_t trigger=TRIGGER_12V) 
+RelayController::RelayController(input_t inputs=NUM_OF_INPUTS) 
 {
     numOfInputs = inputs;
-    triggerType = trigger;
+    setStandardTrigger();
 }
 
 void RelayController::begin()
@@ -24,7 +24,7 @@ void RelayController::begin()
 
 void RelayController::setInput(input_t inputNmbr)
 {
-    for (byte pin = 0; pin < numOfInputs; pin++) 
+    for (byte pin = 3; pin < (numOfInputs + 2); pin++) 
     {
         mcp.digitalWrite(pin, (inputNmbr == pin));
         selectedInput = inputNmbr;
@@ -35,6 +35,41 @@ void RelayController::setInputName(input_t inputNmbr, char* inputName)
 {
 
 }
+
+void RelayController::setAlternateTrigger(uint8_t inputRight, uint8_t inputLeft)
+{
+    sensorRight = inputRight;
+    sensorLeft = inputLeft;
+    standardTrigger = false;
+}
+
+void RelayController::setStandardTrigger()
+{
+    sensorRight = 0;
+    sensorLeft = 0;
+    standardTrigger = true;
+}
+
+void RelayController::setTriggerOn()
+{
+    if (standardTrigger) {
+        mcp.digitalWrite(1,HIGH);
+        mcp.digitalWrite(2, HIGH);
+    } else {
+        // Add logic to handle alternative trigger here
+    }
+}
+
+void RelayController::SetTrifferOff()
+{
+    if (standardTrigger) {
+        mcp.digitalWrite(1,LOW);
+        mcp.digitalWrite(2, LOW);
+    } else {
+        // Add logic to handle alternative trigger here
+    }
+}
+
 
 uint8_t RelayController::getInput()
 {
--- lib/RelayController/RelayController.h
+++ lib/RelayController/RelayController.h
@@ -3,8 +3,7 @@
   A description of the input selector
 
 */
-#define TRIGGER_12V true   
-#define TRIGGER_ALT false
+
 #define NUM_OF_INPUTS 6
 
 #include <Arduino.h>
@@ -18,12 +17,16 @@ public:
 	typedef uint8_t input_t;
 	typedef boolean trigger_t;
 	
-	RelayController(input_t inputs=NUM_OF_INPUTS, trigger_t trigger=TRIGGER_12V);
+	RelayController(input_t inputs=NUM_OF_INPUTS);
 
 	void begin();
 
 	void setInput(input_t inputNmbr);
 	void setInputName(input_t inputNmbr, char* inputName);
+	void setAlternateTrigger(uint8_t inputRight, uint8_t inputLeft);
+	void setStandardTrigger();
+	void setTriggerOn();
+	void SetTrifferOff();
 	
 	input_t getInput();
 	char* getInputName(input_t inputNmbr);
@@ -33,6 +36,9 @@ public:
 private:
 	input_t				selectedInput;
 	input_t				numOfInputs;
+	input_t				sensorRight;
+	input_t				sensorLeft;
+	boolean				standardTrigger;
 	Adafruit_MCP23008 	mcp;
 	trigger_t 			triggerType;
 };
