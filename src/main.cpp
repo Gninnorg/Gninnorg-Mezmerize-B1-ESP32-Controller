f5b2c0e1b519d4376206a08bcf042ca0fbe6693f
Carsten Grønning <carsten.groenning@gmail.com>
2020-01-27 19:54:16 +0100

    added IR and 2nd rotary encoder

--- src/main.cpp
+++ src/main.cpp
@@ -9,73 +9,132 @@
 **
 */
 
-#include <Arduino.h>
+#include "Arduino.h"
 #include "RelayController.h"
 #include "ClickEncoder.h"
 #include "TimerOne.h"
+#include "Muses72320.h"
+#include "IRLremote.h"
 
+// Setup Muses72320
+// The address wired into the muses chip (usually 0).
+static const byte MUSES_ADDRESS = 0;
+static Muses72320 Muses(MUSES_ADDRESS);
+static Muses72320::volume_t CurrentVolume = -20;
+
+
+// Setup relay controller -----------------------------------------------------
 RelayController rc;
 
-ClickEncoder *encoder1 = new ClickEncoder(5, 4, 3, 4);
-ClickEncoder::Button button;
-int16_t last, value;
+void setupMuses72320() {
+  
+  // Initialize muses (SPI, pin modes)...
+  Muses.begin();
 
+  // Muses initially starts in a muted state, set a volume to enable sound.
+  Muses.setVolume(CurrentVolume);
+
+  // These are the default states and could be removed...
+  Muses.setZeroCrossing(true);     // Enable/Disable zero crossing.
+  Muses.setAttenuationLink(false); // Left channel controls both L/R gain channel.
+  Muses.setGainLink(false);        // Left channel controls both L/R attenuation channel.
+
+}
+
+// Setup Rotary encoders ------------------------------------------------------
+#define VERBOSECASE(label) case label: Serial.println(#label); break;
+
+ClickEncoder *encoder1 = new ClickEncoder(5, 4, 3, 4);
+ClickEncoder::Button button1;
+int16_t e1last, e1value;
+
+ClickEncoder *encoder2 = new ClickEncoder(5, 4, 3, 4);
+ClickEncoder::Button button2;
+int16_t e2last, e2value;
 
 void timerIsr() {
   encoder1->service();
+  encoder2->service();
 }
 
-void setup () {
-    Serial.begin(9600);
-    Serial.println(F("Setup()"));
-    rc.begin();
-    rc.setTriggerOn();
-    rc.setInput(1);
-    rc.setInputName(1, F("DAC"));
-    Serial.println(rc.getInputName(rc.getInput()));
-    delay(5000);
-    rc.setInput(2);
-    rc.setInputName(2, F("DAC2"));
-    Serial.println(rc.getInputName(rc.getInput()));
-    delay(1000);
-    rc.setInput(3);
-    delay(1000);
-    rc.setInput(4);
-    delay(1000);
-    rc.setInput(5);
-    delay(1000);
-    rc.setInput(6);
-    Serial.println(rc.getInputName(rc.getInput()));
+void setupRotaryEncoders() {
     Timer1.initialize(1000);
     Timer1.attachInterrupt(timerIsr); 
+}
+
+// Setup IR -------------------------------------------------------------------
+#define pinIR 2
+CHashIR IRLremote;
+
+void setupIR() {
+  if (!IRLremote.begin(pinIR))
+    Serial.println(F("You did not choose a valid pin."));
+}
+
+
+
+// Setup arduino nano ---------------------------------------------------------
+void setup () {
+   while (!Serial);
+  Serial.begin(115200);
+  Serial.println(F("Startup"));
+  //setupMuses72320();
+  setupRotaryEncoders();
+  setupIR();
 }
 
 
 void loop () {
 
-  value += encoder1->getValue();
-    
-  if (value != last) {
-    last = value;
-    Serial.print("Encoder Value: ");
-    Serial.println(value);
+  e1value += encoder1->getValue();
+  if(e1value > 0) e1value= 0; else if(e1value < -223) e1value = -223;
+
+  if (e1value != e1last) {
+    e1last = e1value;
+    Serial.print("Encoder 1 - value : "); Serial.println(e1value);
+    //Muses.setVolume(value);
+    //delay(10);
+  }
+
+  e2value += encoder2->getValue();
+  if (e2value != e2last) {
+    e2last = e2value;
+    Serial.print("Encoder 2 - value : "); Serial.println(e2value);
   }
   
-  button = encoder1->getButton();
-  if (button != ClickEncoder::Open) {
-    Serial.print("Button: ");
-    #define VERBOSECASE(label) case label: Serial.println(#label); break;
-    switch (button) {
-      VERBOSECASE(ClickEncoder::Pressed);
-      VERBOSECASE(ClickEncoder::Held)
-      VERBOSECASE(ClickEncoder::Released)
-      VERBOSECASE(ClickEncoder::Clicked)
-      case ClickEncoder::DoubleClicked:
-          Serial.println("ClickEncoder::DoubleClicked");
-          encoder1->setAccelerationEnabled(!encoder1->getAccelerationEnabled());
-          Serial.print("  Acceleration is ");
-          Serial.println((encoder1->getAccelerationEnabled()) ? "enabled" : "disabled");
-        break;
-    }
-  }    
+  button1 = encoder1->getButton();
+  switch (button1) {
+    case ClickEncoder::Open : break;
+    VERBOSECASE(ClickEncoder::Closed);
+    VERBOSECASE(ClickEncoder::Pressed);
+    VERBOSECASE(ClickEncoder::Held);
+    VERBOSECASE(ClickEncoder::Clicked);
+    VERBOSECASE(ClickEncoder::Released);
+    VERBOSECASE(ClickEncoder::DoubleClicked);
+  }
+  
+  button2 = encoder2->getButton();
+  switch (button2) {
+    case ClickEncoder::Open : break;
+    VERBOSECASE(ClickEncoder::Closed);
+    VERBOSECASE(ClickEncoder::Pressed);
+    VERBOSECASE(ClickEncoder::Held);
+    VERBOSECASE(ClickEncoder::Clicked);
+    VERBOSECASE(ClickEncoder::Released);
+    VERBOSECASE(ClickEncoder::DoubleClicked);
+  }
+
+  if (IRLremote.available())
+  {
+    // Get the new data from the remote
+    auto data = IRLremote.read();
+
+    // Print the protocol data
+    Serial.print(F("Address: 0x"));
+    Serial.println(data.address, HEX);
+    Serial.print(F("Command: 0x"));
+    Serial.println(data.command, HEX);
+    Serial.println();
+  }
+     
 }
